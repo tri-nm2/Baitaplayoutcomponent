a584bcde77f99ae4e8b538df4632f302f89f14f8
tri-nm2 <nguyenminhtri19031994@gmail.com>
2022-07-15 12:03:23 +0700

    feature: implement project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/baitaplayoutcomponent/navbarComponent/navbar";
+import Header from "./components/baitaplayoutcomponent/headerComponent/header";
+import FeatureList from "./components/baitaplayoutcomponent/featureListComponent/featureList";
+import Footer from "./components/baitaplayoutcomponent/footerComponent/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Header />
+      <FeatureList />
+      <Footer />
     </div>
   );
 }
